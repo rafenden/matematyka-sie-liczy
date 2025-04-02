68de6d0cd0793c8e578495fdc3749cf546e2e4fd
Rafal Enden <git@enden.com>
2025-04-02 20:02:47 +0200

    Update layout and logo

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = function(eleventyConfig) {
     // Copy `css/` to `_site/css/`
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("images");
+    
+    // Add shortcode for current year
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
     return {
         // Set directories to pass through to the _site folder
